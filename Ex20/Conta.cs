5b1e495a4f8a84ec16cb579c8a7761d55b7e66da
Gustavo Morais <gustavo.morais3119@gmail.com>
2024-03-18 17:44:44 -0300

    Add project files.

---
+++ Ex20/Conta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex20
+{
+    internal class Conta
+    {
+        public int NumeroDaConta;
+        public string Nome;
+        public double Saldo;
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Retirar(double valor)
+        {
+            Saldo -= valor;
+        }
+    }
+}
---
+++ Ex20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Ex20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+            Console.Write("Digite o número da conta: ");
+            c.NumeroDaConta = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite o titular da conta: ");
+            c.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite o saldo da conta: ");
+            c.Saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Digite o valor do depósito: ");
+            double deposito = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(deposito);
+            Console.WriteLine("Depósito realizado com sucesso!");
+            Console.WriteLine();
+            Console.Write("Digite o valor do saque: ");
+            deposito = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Retirar(deposito);
+            Console.WriteLine("Saque realizado com sucesso!");
+            Console.WriteLine();
+            Console.Write("Saque realizado com sucesso! "+ c.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
